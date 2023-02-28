790d24e0a704da6fa6847a01012a2eae62232132
Nyssa Silbiger <nyssa.silbiger@csun.edu>
2023-02-28 11:49:21 -0800

    created my first function

---
+++ R/my_function.R
@@ -0,0 +1,14 @@
+#' my_function
+#'
+#' This function will add two parameters together and return the sum
+#'
+#' @param parameter_one Numerical value added to parameter_two
+#' @param parameter_two Numerical value added to param_one
+#' @return Sum of two parameters will be returned
+#' @export
+my_function <- function(parameter_one, parameter_two){
+
+  my_sum <- parameter_one + parameter_two
+
+  return(my_sum)
+}
